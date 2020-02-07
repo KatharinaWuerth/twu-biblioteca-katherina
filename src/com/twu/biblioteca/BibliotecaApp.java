47f4324b36995d95eeb65d39b6638517e431810e
KatharinaWuerth <K.Wuerth@gmx.net>
2020-02-07 17:50:02 +0100

    Print List of Books and add test for method getListOfBooks

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,11 @@
 package com.twu.biblioteca;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static String welcomeMessage(){
@@ -8,7 +14,14 @@ public class BibliotecaApp {
         return message;
     }
 
-    public static void main(String[] args) {
+    public static List getListOfBooks() throws IOException {
+        List<String> books = Files.readAllLines(Paths.get("/Users/katharinawuerth/Desktop/TWU_Biblioteca-master/src/com/twu/biblioteca/listOfBooks.txt"), StandardCharsets.UTF_8);
+        return books;
+    }
+
+    public static void main(String[] args) throws IOException {
         welcomeMessage();
+        List<String> books  = getListOfBooks();
+        System.out.println(books);
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -3,6 +3,13 @@ package com.twu.biblioteca;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -10,5 +17,11 @@ public class ExampleTest {
     public void shouldReturnWelcomeMessage() {
         String greeting = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
         assertEquals(BibliotecaApp.welcomeMessage(), greeting);
+    }
+
+    @Test
+    public void shouldShowListOfBooks() throws IOException {
+        List<String> books = Files.readAllLines(Paths.get("/Users/katharinawuerth/Desktop/TWU_Biblioteca-master/src/com/twu/biblioteca/listOfBooks.txt"), StandardCharsets.UTF_8);
+        assertEquals(BibliotecaApp.getListOfBooks(), books);
     }
 }
