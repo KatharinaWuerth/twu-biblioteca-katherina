15cb35b7dbb23ca43a3dae3e04fc03c2f3b3ad67
KatharinaWuerth <K.Wuerth@gmx.net>
2020-02-07 17:13:34 +0100

    Print welcome massage and add test for method welcomeMessage

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,13 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public static String welcomeMessage(){
+        String message = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        System.out.println(message);
+        return message;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        welcomeMessage();
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,14 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
-
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldReturnWelcomeMessage() {
+        String greeting = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        assertEquals(BibliotecaApp.welcomeMessage(), greeting);
     }
 }
